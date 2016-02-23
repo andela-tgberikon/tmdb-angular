097fa056f1d7710840a04b2dc63e4d71a3d54d3d
Terwase Gberikon <terwase@live.com>
2016-02-23 16:07:52 +0100

    Moved global object variables into TMDB.config, the CORS problem occured because the url wasn\'t being built properly.

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
-angular.module('TMDB', [])
-  .controller('moviesController', ['$http', '$scope', function($http, $scope) {
-    var TMDBparams = {
-      baseSearchMovie: "https://api.themoviedb.org/3/search/movie",
-      base2: "https://api.themoviedb.org/3/movie/popular",
-      baseUrl: "",
-      youtubeUrl: "http://www.youtube.com/embed/",
-      baseMovieTrailer: "https://api.themoviedb.org/3/movie/",
-      params: { api_key: "?api_key=fb5a22ba28e3a8b761c623cb071fa7a9" }
-    };
-    $scope.yem = function() {
-      console.log("Yeet!", TMDBparams.base2 + TMDBparams.params.api_key);
-    };
-    $scope.popularMovies = function() {
-      $http.jsonp('TMDBparams.base2 + TMDBparams.params.api_key').success(function(res, req) {
-        console.log('This is the success response: ', res);
-        console.log('This is the success request: ', req)
-      }).error(function(res, req) {
-        console.log('This is the error response: ', res);
-        console.log('This is the error request: ', req)
-      })
+var TMDB = angular.module('TMDB', []);
+
+TMDB.config(function($httpProvider) {
+  return TMDBparams = {
+    baseSearchMovie: "https://api.themoviedb.org/3/search/movie",
+    base2: 'https://api.themoviedb.org/3/movie/popular',
+    baseUrl: "",
+    youtubeUrl: "http://www.youtube.com/embed/",
+    baseMovieTrailer: "https://api.themoviedb.org/3/movie/",
+    params: {
+      api_key: "?api_key=fb5a22ba28e3a8b761c623cb071fa7a9",
+      page: 0
     }
-  }]);
+  };
+});
+
+TMDB.controller('moviesController', ['$http', '$scope', function($http, $scope) {
+  $scope.yem = function() {
+    console.log("Yeet!", TMDBparams.base2);
+    myMovies.load();
+  };
+  $scope.popularMovies = function() {
+    $http.get(TMDBparams.base2 + TMDBparams.params.api_key).then(function(res, req) {
+      console.log('This is the success response: ', res);
+      console.log('This is the success request: ', req)
+    });
+  }
+}]);
