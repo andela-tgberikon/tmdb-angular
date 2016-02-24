312af9964fc2bed4a0afddf0bcc2114f7b197940
Terwase Gberikon <terwase@live.com>
2016-02-24 18:54:38 +0100

    Refactored code to remove duplicates in moviesController for popular movies function call and pagination function call

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,8 +1,16 @@
 angular.module('TMDB.controllers', [])
   .controller('moviesController', ['$scope', 'getMovies', function($scope, getMovies) {
+    var paginateObject = {
+      getPopularMovies: function() {
+        getMovies.popularMovies(function(movies) {
+          $scope.popular = true;
+          $scope.showcase = false;
+          $scope.displayMovies = movies.results;
+        });
+      }
+    }
     $scope.oneMovie = function(movie) {
       getMovies.getOneMovie(movie, function(movie) {
-        console.log(movie, 'This is the movie');
         /* The trailer URL is a different API call which would produce the youtube link that we would have to embed 
             in an iFrame or a html5 video player, preferrably an overlay.
             Also, reviews are a separate API call, which would be added to properties of the video.
@@ -12,11 +20,7 @@ angular.module('TMDB.controllers', [])
         $scope.displayMovies = '';
       });
     };
-    $scope.popularMovies = getMovies.popularMovies(function(movies) {
-      $scope.popular = true;
-      $scope.showcase = false;
-      $scope.displayMovies = movies.results;
-    });
+    $scope.popularMovies = paginateObject.getPopularMovies();
     $scope.search = function(movieName) {
       if (angular.isUndefined(movieName)) {
         return false;
@@ -30,43 +34,28 @@ angular.module('TMDB.controllers', [])
         });
       }
     };
+
     $scope.nextPage = function() {
       if ((TMDBparams.storedMovies.page + 1) > (TMDBparams.storedMovies.total_pages)) {
         return false;
       } else {
-        console.log(TMDBparams.storedMovies.page, 'This is the current page');
         TMDBparams.storedMovies.page = TMDBparams.storedMovies.page + 1;
         TMDBparams.params.page = TMDBparams.storedMovies.page;
-        console.log(TMDBparams.params.page);
-        console.log(TMDBparams.storedMovies, 'nextPage storedMovies');
-        getMovies.popularMovies(function(movies) {
-          $scope.popular = true;
-          $scope.showcase = false;
-          $scope.displayMovies = movies.results;
-        });
+        paginateObject.getPopularMovies();
         TMDBparams.storedMovies = '';
       }
     };
     $scope.previousPage = function() {
       if ((TMDBparams.storedMovies.page - 1) == 0) {
-        console.log('Exceeded the starting number');
         return false;
       } else {
-        console.log(TMDBparams.storedMovies.page - 1, 'Going back one page');
-        console.log(TMDBparams.storedMovies, 'this is TMDBparams.storedMovies');
-         TMDBparams.storedMovies.page = TMDBparams.storedMovies.page - 1;
-         TMDBparams.params.page = TMDBparams.storedMovies.page;
-         console.log(TMDBparams.storedMovies, 'previousPage storedMovies');
-         getMovies.popularMovies(function(movies) {
-          $scope.popular = true;
-          $scope.showcase = false;
-          $scope.displayMovies = movies.results;
-        });
-         TMDBparams.storedMovies = '';
+        TMDBparams.storedMovies.page = TMDBparams.storedMovies.page - 1;
+        TMDBparams.params.page = TMDBparams.storedMovies.page;
+        paginateObject.getPopularMovies();
+        TMDBparams.storedMovies = '';
       }
     };
     $scope.backToSearch = function() {
-      console.log(TMDBparams.storedMovies, 'this is the storedMovies from backToSearch');
       $scope.displayMovies = TMDBparams.storedMovies.results;
       $scope.popular = true;
       $scope.showcase = false;
--- scripts/services.js
+++ scripts/services.js
@@ -3,7 +3,6 @@ angular.module('TMDB.services', [])
     return {
       popularMovies: function(movieCallBack) {
         if (TMDBparams.params.page > 1) {
-          console.log(TMDBparams.storedMovies, 'After being paginated');
           $http.get(TMDBparams.base2 + TMDBparams.params.api_key + '&page=' + TMDBparams.params.page).then(function(response, request) {
             TMDBparams.storedMovies = response.data;
             return movieCallBack(response.data);
@@ -11,26 +10,21 @@ angular.module('TMDB.services', [])
         } else {
           $http.get(TMDBparams.base2 + TMDBparams.params.api_key).then(function(response, request) {
             TMDBparams.storedMovies = response.data;
-          console.log(TMDBparams.storedMovies, 'Before being paginated');
             return movieCallBack(response.data);
           });
         }
       },
       getOneMovie: function(movieID, movieCallBack) {
         $http.get(TMDBparams.base1 + movieID + TMDBparams.params.api_key).then(function(response, request) {
-          console.log(TMDBparams.storedMovies, 'This is storedMovies');
           return movieCallBack(response.data);
         });
       },
       findMovie: function(movieName, movieCallBack) {
         $http.get(TMDBparams.baseSearchMovie + TMDBparams.params.api_key + '&query=' + movieName).then(function(response, request) {
           if (response.data.total_results == 0) {
-            console.log(false, "It's a lie, no movie typed");
             return false;
           } else {
-            // console.log(response.data, 'from findMovie');
             TMDBparams.storedMovies = response.data;
-            console.log(TMDBparams.storedMovies, 'from findMovie TMDBparams.storedMovies');
             return movieCallBack(response.data);
           }
         });
